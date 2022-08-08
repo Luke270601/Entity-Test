2a9f89adcb5e45c5ef924e45af9d10ef1bed7aaf
Luke Scott <77630341+Luke270601@users.noreply.github.com>
2022-08-09 00:13:44 +0100

    Added entity renderer

--- src/main/java/net/lukescott/entitytest/entity/client/CrocoLizardRenderer.java
+++ src/main/java/net/lukescott/entitytest/entity/client/CrocoLizardRenderer.java
@@ -1,4 +1,19 @@
 package net.lukescott.entitytest.entity.client;
 
-public class CrocoLizardRenderer {
+import net.lukescott.entitytest.EntityTestMod;
+import net.lukescott.entitytest.entity.custom.CrocoLizardEntity;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.util.Identifier;
+import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
+
+public class CrocoLizardRenderer extends GeoEntityRenderer<CrocoLizardEntity> {
+    public CrocoLizardRenderer(EntityRendererFactory.Context ctx, AnimatedGeoModel<CrocoLizardEntity> modelProvider) {
+        super(ctx, new CrocoLizardModel());
+    }
+
+    @Override
+    public Identifier getTextureResource(CrocoLizardEntity instance) {
+        return new Identifier(EntityTestMod.MOD_ID, "textures/entity/crocolizard/texture.png");
+    }
 }
